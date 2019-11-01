edfa3d9c4820b182c97e7cc63d1bd74bdab96887
shinz <sethhinz@me.com>
2019-11-01 12:03:51 -0700

    Formatted specs

--- spec/phreak/comprehensive_spec.cr
+++ spec/phreak/comprehensive_spec.cr
@@ -2,41 +2,41 @@ require "../../src/*"
 require "spec"
 
 describe Phreak do
-	describe "#parse" do
-		it "correctly reaches a multilevel endpoint" do
-			reached = false
+  describe "#parse" do
+    it "correctly reaches a multilevel endpoint" do
+      reached = false
 
-			Phreak.parse("alpha -g l".split(" ")) do |root|
-				root.bind(word: "alpha") do |sub|
-					sub.bind(short_flag: 'g') do |sub|
-						sub.bind(word: "l") do |sub|
-							reached = true
-						end
-					end
-				end
-			end
+      Phreak.parse("alpha -g l".split(" ")) do |root|
+        root.bind(word: "alpha") do |sub|
+          sub.bind(short_flag: 'g') do |sub|
+            sub.bind(word: "l") do |sub|
+              reached = true
+            end
+          end
+        end
+      end
 
-			reached.should eq true
-		end
+      reached.should eq true
+    end
 
-		it "Correctly executes a flag stack" do
-			flags = [] of Char
+    it "Correctly executes a flag stack" do
+      flags = [] of Char
 
-			Phreak.parse("-xyz".split(" ")) do |root|
-				root.bind(short_flag: 'x') do |sub|
-					flags.push 'x'
-				end
+      Phreak.parse("-xyz".split(" ")) do |root|
+        root.bind(short_flag: 'x') do |sub|
+          flags.push 'x'
+        end
 
-				root.bind(short_flag: 'y') do |sub|
-					flags.push 'y'
-				end
+        root.bind(short_flag: 'y') do |sub|
+          flags.push 'y'
+        end
 
-				root.bind(short_flag: 'z') do |sub|
-					flags.push 'z'
-				end
-			end
+        root.bind(short_flag: 'z') do |sub|
+          flags.push 'z'
+        end
+      end
 
-			flags.should eq ['x', 'y', 'z']
-		end
-	end
+      flags.should eq ['x', 'y', 'z']
+    end
+  end
 end
--- spec/phreak/subparser_spec.cr
+++ spec/phreak/subparser_spec.cr
@@ -2,111 +2,111 @@ require "../../src/*"
 require "spec"
 
 describe Phreak::Subparser do
-	describe "#bind" do
-		it "correctly binds to flags" do
-			count = 0
+  describe "#bind" do
+    it "correctly binds to flags" do
+      count = 0
 
-			Phreak.parse("test --test -t".split(" ")) do |root|
-				root.bind(word: "test", long_flag: "test", short_flag: 't') do
-					count += 1
-				end
-			end
+      Phreak.parse("test --test -t".split(" ")) do |root|
+        root.bind(word: "test", long_flag: "test", short_flag: 't') do
+          count += 1
+        end
+      end
 
-			count.should eq 3
-		end
+      count.should eq 3
+    end
 
-		it "raises an UnrecognizedTokenException when token not recognized" do
-			expect_raises(Phreak::UnrecognizedTokenException) do
-				Phreak.parse("foo".split(" ")) do |root|
-					root.bind(word: "bar") do
-						raise Exception.new("Should not have reached this point!")
-					end
-				end
-			end
-		end
+    it "raises an UnrecognizedTokenException when token not recognized" do
+      expect_raises(Phreak::UnrecognizedTokenException) do
+        Phreak.parse("foo".split(" ")) do |root|
+          root.bind(word: "bar") do
+            raise Exception.new("Should not have reached this point!")
+          end
+        end
+      end
+    end
 
-		it "handles multilevel binding" do
-			reached = false
+    it "handles multilevel binding" do
+      reached = false
 
-			Phreak.parse("foo bar".split(" ")) do |root|
-				root.bind(word: "foo") do |sub|
-					sub.bind(word: "bar") do |sub|
-						reached = true
-					end
+      Phreak.parse("foo bar".split(" ")) do |root|
+        root.bind(word: "foo") do |sub|
+          sub.bind(word: "bar") do |sub|
+            reached = true
+          end
 
-					sub.bind(word: "baz") do |sub|
-						raise Exception.new("Should not have reached this point!")
-					end
-				end
-			end
+          sub.bind(word: "baz") do |sub|
+            raise Exception.new("Should not have reached this point!")
+          end
+        end
+      end
 
-			reached.should eq true
-		end
-	end
+      reached.should eq true
+    end
+  end
 
-	describe "#fuzzy_bind" do
-		it "correctly fuzzy matches" do
-			recognized = 0
-			unrecognized = 0
+  describe "#fuzzy_bind" do
+    it "correctly fuzzy matches" do
+      recognized = 0
+      unrecognized = 0
 
-			Phreak.parse("fuzzy fuzz fuz fu f".split(" ")) do |root|
-				root.fuzzy_bind(word: "fuzzy") do
-					recognized += 1
-				end
+      Phreak.parse("fuzzy fuzz fuz fu f".split(" ")) do |root|
+        root.fuzzy_bind(word: "fuzzy") do
+          recognized += 1
+        end
 
-				root.unrecognized_args do
-					unrecognized += 1
-				end
-			end
+        root.unrecognized_args do
+          unrecognized += 1
+        end
+      end
 
-			recognized.should eq 4
-			unrecognized.should eq 1
-		end
-	end
+      recognized.should eq 4
+      unrecognized.should eq 1
+    end
+  end
 
-	describe "#grab" do
-		it "gets a wildcard token" do
-			Phreak.parse("token".split(" ")) do |root|
-				root.grab do |sub, token|
-					token.should eq "token"
-				end
-			end
-		end
-	end
+  describe "#grab" do
+    it "gets a wildcard token" do
+      Phreak.parse("token".split(" ")) do |root|
+        root.grab do |sub, token|
+          token.should eq "token"
+        end
+      end
+    end
+  end
 
-	describe "#missing_args" do
-		it "calls back when an argument that does not exist is accessed" do
-			ran = false
+  describe "#missing_args" do
+    it "calls back when an argument that does not exist is accessed" do
+      ran = false
 
-			Phreak.parse(["subcommand"]) do |root|
-				root.bind(word: "subcommand") do |sub|
-					sub.grab do
-					end
-				end
+      Phreak.parse(["subcommand"]) do |root|
+        root.bind(word: "subcommand") do |sub|
+          sub.grab do
+          end
+        end
 
-				root.missing_args do
-					ran = true
-				end
-			end
+        root.missing_args do
+          ran = true
+        end
+      end
 
-			ran.should eq true
-		end
-	end
+      ran.should eq true
+    end
+  end
 
-	describe "#unrecognized_args" do
-		it "calls back when an unrecognized argument is encountered" do
-			count = 0
+  describe "#unrecognized_args" do
+    it "calls back when an unrecognized argument is encountered" do
+      count = 0
 
-			Phreak.parse("foo bar baz foo".split(" ")) do |root|
-				root.bind(word: "foo") do
-				end
+      Phreak.parse("foo bar baz foo".split(" ")) do |root|
+        root.bind(word: "foo") do
+        end
 
-				root.unrecognized_args do
-					count += 1
-				end
-			end
+        root.unrecognized_args do
+          count += 1
+        end
+      end
 
-			count.should eq 2
-		end
-	end
+      count.should eq 2
+    end
+  end
 end
